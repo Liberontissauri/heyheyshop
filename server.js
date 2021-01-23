78fffa1fd114cfb9a976104c3e063154fdc6298a
Liberontissauri <jp.liberon@gmail.com>
2021-01-23 15:34:26 +0000

    static file route added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.use("/static", express.static("src"));
 
 app.listen(port, () => {
     console.log(`The server has started successfully and is running on port: ${port}`);
